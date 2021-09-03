83dea557b23765ff0556174b95127f353736fe19
vinanrra <vicentefernandezguerra@gmail.com>
2021-09-03 17:40:23 +0200

    Added Radarr and Sonarr improvements
    Now, the script will not back up MediaCover to avoid big backups

--- Scripts/Backup/backupV3.sh
+++ Scripts/Backup/backupV3.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
-
-# Backup folder
-dir=XXXXXXXXXXXX
-
-# Folder where are the folders to backup
-dirHome=XXXXXXXXXXXX
-
-# Folders to backup, case-sensitive
+dir=<PATH_TO_SAVE_BACKUPS>
+dirHome=<PATH_FOLDERS_TO_BACKUP>
 carpetas=(
-  XXXXXXXXXXXX
-  XXXXXXXXXXXX
+  folder1
+  folder2
+  folder3
 )
 
-# Start a bucle with each folder and finish when no more folders
 for index in ${!carpetas[*]}; do 
 
-	# Create folders if not exist
-    if [ -d $dir/${carpetas[$index]} ]
-    then
-        echo ${carpetas[$index]} - La carpeta ya existe
-    else
-        # Create folder and parent folders
-        mkdir -p $dir/${carpetas[$index]}
-    fi
-
-    # Exception, if folder is Plex
     if [ ${carpetas[$index]} = Plex ];
     then
         echo "
@@ -35,7 +19,6 @@ for index in ${!carpetas[*]}; do
 
             =======================================================================
             "
-        # Create a copy of the sqlite database and add current date to file 
         echo ".dump metadata_item_settings" | sqlite3 $dirHome/Plex/config/Library/Application\ Support/Plex\ Media\ Server/Plug-in\ Support/Databases/com.plexapp.plugins.library.db | grep -v TABLE | grep -v INDEX > $dir/Plex/$(date +%m.%d.%H.%M)PlexDatabase.sql
         echo "
             =======================================================================
@@ -56,7 +39,6 @@ for index in ${!carpetas[*]}; do
 
             =======================================================================
             "
-        # Create a copy of the sqlite database and add current date to file 
         sqlite3 $dirHome/Nginx_SSL/config/www/Kanboard/data/db.sqlite ".backup $dir/Kanboard/Kanboard.$(date +%m.%d.%H.%M).data.sqlite"
         echo "
             =======================================================================
@@ -77,7 +59,6 @@ for index in ${!carpetas[*]}; do
 
             =======================================================================
             "
-        # Export crontab list to file and add current date.
         crontab -l > $dir/${carpetas[$index]}/Crontab.$(date +%m.%d.%H.%M).txt
         echo "
             =======================================================================
@@ -87,7 +68,46 @@ for index in ${!carpetas[*]}; do
 
             =======================================================================
             "
-    # If is not an exception backup folders inside folder called with software name
+    elif [ ${carpetas[$index]} = Sonarr ]
+    then
+        echo "
+            =======================================================================
+            IMPORTANTANTE:
+            
+            Copia de ${carpetas[$index]} iniciada.
+
+            =======================================================================
+            "
+        tar --exclude='Sonarr/config/MediaCover' -C $dirHome -pzvcf $dir/${carpetas[$index]}/${carpetas[$index]}.$(date +%m.%d.%H.%M).tar.gz ${carpetas[$index]}
+        echo "
+            =======================================================================
+            IMPORTANTANTE:
+            
+            Copia de ${carpetas[$index]} finalizada.
+
+            =======================================================================
+            "
+   
+    elif [ ${carpetas[$index]} = Radarr ]
+    then
+        echo "
+            =======================================================================
+            IMPORTANTANTE:
+            
+            Copia de ${carpetas[$index]} iniciada.
+
+            =======================================================================
+            "
+        tar --exclude='Radarr/config/MediaCover' -C $dirHome -pzvcf $dir/${carpetas[$index]}/${carpetas[$index]}.$(date +%m.%d.%H.%M).tar.gz ${carpetas[$index]}
+        echo "
+            =======================================================================
+            IMPORTANTANTE:
+            
+            Copia de ${carpetas[$index]} finalizada.
+
+            =======================================================================
+            "
+   
     else
         echo "
             =======================================================================
@@ -97,7 +117,6 @@ for index in ${!carpetas[*]}; do
 
             =======================================================================
             "
-        # Backup in tar.gz with current date
         tar -C $dirHome -pzvcf $dir/${carpetas[$index]}/${carpetas[$index]}.$(date +%m.%d.%H.%M).tar.gz ${carpetas[$index]}
         echo "
             =======================================================================
@@ -116,8 +135,7 @@ for index in ${!carpetas[*]}; do
             Borrado de archivos de mas de 7 dias iniciado de ${carpetas[$index]}.
 
             =======================================================================
-         "
-    # Remove files which are 7 days old
+            "
     find $dir/${carpetas[$index]}/ -mtime +7 -exec rm {} \;
     echo "
             =======================================================================
