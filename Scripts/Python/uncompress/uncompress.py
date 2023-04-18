4fa6f139108f674d40117f9ceba8577590ecb8c3
Vicente Fernández Guerra <vicentefernandezguerra@gmail.com>
2023-04-18 20:18:23 +0200

    Update uncompress.py

--- Scripts/Python/uncompress/uncompress.py
+++ Scripts/Python/uncompress/uncompress.py
@@ -1,9 +1,14 @@
-import patoolib, glob, sys
+import patoolib
+import glob
+import sys
+import shutil
+import os
 
 # Usage: python uncompress.py PATH
 # Example: python uncompress.py /path/to/folder/compressed.zip
 
 rootFolder = sys.argv[1]
+tempFolder = "/tmp"
 # List of compressed files
 # Need unzip and unrar installed
 types = ["zip", "rar"]
@@ -15,5 +20,11 @@ for type in types:
 
   # Uncompress all files found at the folder
   for i in files:
-    # Uncompress each file in same dir, without showing any info and overwrite if already uncompressed
-    patoolib.extract_archive(i, outdir=rootFolder, verbosity=-1, interactive=False)
+    # Uncompress each file in the same dir, without showing any info and overwrite if already uncompressed
+    patoolib.extract_archive(i, outdir=tempFolder, verbosity=-1, interactive=False)
+
+  # Move all uncompressed files to the root folder
+  for file in glob.glob(tempFolder + "/*.mkv"):
+    shutil.move(file, os.path.join(rootFolder, os.path.basename(file)))
+
+print(f"[SCRIPT] Uncompressed {rootFolder}")
