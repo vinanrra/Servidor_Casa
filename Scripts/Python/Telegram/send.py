d048f451be6ef3c789f70ab7e265cbd0115ec506
Vicente Fernández Guerra <vicentefernandezguerra@gmail.com>
2024-03-28 20:19:50 +0100

    Update send.py

--- Scripts/Python/Telegram/send.py
+++ Scripts/Python/Telegram/send.py
@@ -11,29 +11,30 @@ CHAT_ID = 'CHAT_ID_HERE'
 # Folder containing the photos
 FOLDER_PATH = 'FOLDER_PATH_HERE'
 
+# Message with the media
+CAPTION = "⚠ Movimiento detectado"
+
 # Function to send photos to Telegram
 async def send_photos_to_telegram(photos):
     bot = telegram.Bot(token=TOKEN)
     media = [telegram.InputMediaPhoto(open(photo, 'rb')) for photo in photos]
     try:
-        await bot.send_media_group(chat_id=CHAT_ID, media=media)
+        await bot.send_media_group(chat_id=CHAT_ID, media=media, caption=CAPTION)
     except Exception as e:
         print("Error sending photos:", e)
 
-# Function to get the last five photos
-def get_last_five_photos(folder):
+# Function to get the last X photos
+def get_last_photos(folder):
     # Get all jpg files recursively
     all_photos = sorted(glob.glob(os.path.join(folder, '**/*.jpg'), recursive=True), key=os.path.getmtime, reverse=True)
-    # Take the last five photos
-    last_five_photos = all_photos[:7]
-    return last_five_photos
+    # Take the last X photos
+    photos = all_photos[:7]
+    return photos
 
-# Main function
 async def main():
-    # Get the last five photos
-    last_five_photos = get_last_five_photos(FOLDER_PATH)
-    # Send the photos to Telegram
-    await send_photos_to_telegram(last_five_photos)
+    photos = get_last_photos(FOLDER_PATH)
+    await send_photos_to_telegram(photos)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
